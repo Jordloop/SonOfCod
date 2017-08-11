d5a9fe2acae3bcbd7f111a8160109c4ba1822db6
JordLoop <Jordloop@gmail.com>
2017-08-11 09:18:32 -0700

    add AdminController

---
+++ src/SonOfCod/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCod.Models;
+
+
+namespace SonOfCod.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationAdmin> _userManager;
+        private readonly SignInManager<ApplicationAdmin> _signInManager;
+
+        public AdminController(UserManager<ApplicationAdmin> userManager, SignInManager<ApplicationAdmin> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
