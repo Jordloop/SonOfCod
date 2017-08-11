bf5d3c1c4f30596fe17a742400e7f1498de2b346
JordLoop <Jordloop@gmail.com>
2017-08-11 09:02:28 -0700

    add dependencies and tools, created ApplicationAdmin and ApplicationDbContext

---
+++ src/SonOfCod/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SonOfCod.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationAdmin>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
