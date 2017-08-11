b215b66095cf395c9533fb2b1acf7dafc7e1cee9
JordLoop <Jordloop@gmail.com>
2017-08-11 15:31:22 -0700

    add  test for Modelsubscriber

---
+++ SonOfCod.Tests/SubscriberTest.cs
@@ -0,0 +1,26 @@
+﻿using SonOfCod.Models;
+using Xunit;
+
+
+namespace SonOfCod.Tests
+{
+    public class SubscriberTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var subscriber = new Subscriber();
+
+            //Act
+            var firstNameResult = subscriber.FirstName = "Indrid";
+            var lastNameResult = subscriber.LastName = "Cold";
+            var companyNameResult = subscriber.CompanyName = "Company Inc.";
+
+            //Assert
+            Assert.Equal("Indrid", firstNameResult);
+            Assert.Equal("Cold", lastNameResult);
+            Assert.Equal("Company Inc.", companyNameResult);
+        }
+    }
+}
