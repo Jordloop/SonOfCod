cec65c4032a32f12bd6fac76b801dbe083fe46b0
JordLoop <Jordloop@gmail.com>
2017-08-11 15:42:56 -0700

    add tests for Model.Subscriber and each View check whether the user is authenticated

---
+++ SonOfCod.Tests/ControllerTest/MarketingControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Controllers;
+using SonOfCod.Models;
+using Xunit;
+namespace SonOfCod.Tests.ControllerTest
+{
+    public class MarketingControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            MarketingController controller = new MarketingController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ SonOfCod.Tests/ControllerTest/NewsletterControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Controllers;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ControllerTest
+{
+    public class NewsletterControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            NewsletterController controller = new NewsletterController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
