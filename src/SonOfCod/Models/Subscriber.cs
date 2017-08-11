f62bc67a442c07a20cbe729cd3631ff6fc7aeb8e
JordLoop <Jordloop@gmail.com>
2017-08-11 09:15:17 -0700

    add Subscriber model

---
+++ src/SonOfCod/Models/Subscriber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SonOfCod.Models
+{
+    [Table("Subscribers")]
+    public class Subscriber
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
